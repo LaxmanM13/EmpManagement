f9fffe560546db40a88096e506d4cfee89235400
Laxman <laxmanmule13@gmail.com>
2022-05-27 20:19:03 +0530

    second commit

--- src/main/java/com/EmployeeManagement/EmpManagement/EmpManagementApplication.java
+++ src/main/java/com/EmployeeManagement/EmpManagement/EmpManagementApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmpManagementApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(EmpManagementApplication.class, args);
 	}
 
