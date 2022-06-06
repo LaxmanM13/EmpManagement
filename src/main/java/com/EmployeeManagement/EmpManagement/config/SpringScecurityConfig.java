4624ebb8ce558eadd6d27ba088556ac9c3f565a8
Laxman <laxmanmule13@gmail.com>
2022-06-06 20:14:28 +0530

    initial commit

--- src/main/java/com/EmployeeManagement/EmpManagement/config/SpringScecurityConfig.java
+++ src/main/java/com/EmployeeManagement/EmpManagement/config/SpringScecurityConfig.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpEntity;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @SuppressWarnings("deprecation")
+@EnableWebSecurity
 @Configuration
 public class SpringScecurityConfig extends WebSecurityConfigurerAdapter
 {
@@ -23,14 +26,11 @@ protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 	 http.csrf().disable();
 	 http.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic();
 	}
- 
- 
-@Bean
- public static NoOpPasswordEncoder passwordEncoder()
- {
- return(NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
- }
 
- 
+
+ @Bean
+ public BCryptPasswordEncoder passwordEncoder() {
+     return new BCryptPasswordEncoder();
+ }
 
 }
